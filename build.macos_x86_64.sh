57d8df32a2dfae0ff296fc6b8ef832f1d7c9aea4
soudegesu <soudegesu>
2021-07-30 11:48:57 +0900

    スレッドの優先度を変更して配信時のパフォーマンスを上げる

--- build.macos_x86_64.sh
+++ build.macos_x86_64.sh
@@ -28,6 +28,7 @@ pushd $SOURCE_DIR/webrtc/src
   patch -p2 < $SCRIPT_DIR/patches/macos_screen_capture.patch
   patch -p1 < $SCRIPT_DIR/patches/macos_simulcast.patch
   patch -p1 < $SCRIPT_DIR/patches/ios_simulcast.patch
+  patch -p1 < $SCRIPT_DIR/patches/ios_thread_priority.patch
 popd
 
 pushd $SOURCE_DIR/webrtc/src
